c7e6bc2639708acd31a719081318faf5f2785876
primeeventudaipur <primeeventudaipur@gmail.com>
2026-07-19 12:39:47 +0530

    Fix sitemap URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://primeeventudaipur.com";
+  const baseUrl = "https://prime-event-mauve.vercel.app";
 
   return [
     {
